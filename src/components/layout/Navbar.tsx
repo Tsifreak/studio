8f09419a2ee994b28c6dc9798a8cba1e2d09b262
Αντρέας Τσιφρήκας <tsifrikas.a@gmail.com>
2025-06-03 12:47:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Navbar>
    
    with the text content:
    
        ΥπηρεσίεςΠώς ΛειτουργείΓιατί Εμάς  Σύνδεση Εγγραφή
    
    Relevant files:
    
        - https://6000-firebase-studio-1747039846284.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_a214a685._.js
        - https://6000-firebase-studio-1747039846284.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    make the navbar background white

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -50,7 +50,7 @@ export function Navbar() {
   ];
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-white backdrop-blur supports-[backdrop-filter]:bg-white/60">
       <div className="container flex h-16 items-center justify-between">
         <Logo iconSize={100} className="ml-2" />
 
